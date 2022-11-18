5eb67da2581cfef6b2e6a3c4e49be96c41886782
Misode <Misoloo64@gmail.com>
2022-11-18 07:48:27 +0100

    Add selection box and moving multiple points

--- src/components/SplineEditor.tsx
+++ src/components/SplineEditor.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useMemo, useRef } from 'preact/hooks'
+import { StateUpdater, useCallback, useMemo, useRef, useState } from 'preact/hooks'
 import { clamp, distance, safeSlope } from '../util'
 
 export interface Point {
@@ -14,18 +14,27 @@ export interface Region {
 	y2: number
 }
 
-interface Drag {
+type Drag = {
 	type: 'point' | 'handle'
 	index: number
+	rels?: Map<number, { x: number, y: number }>
+} | {
+	type: 'selection'
+	x: number
+	y: number
 }
 
 interface Props {
 	region: Region
 	points: Point[]
-	setPoints: (points: Point[]) => void
+	setPoints: StateUpdater<Point[]>
 	axis?: boolean
 }
 export function SplineEditor({ region, points, setPoints, axis }: Props) {
+	const [box, setBox] = useState<Region | undefined>(undefined)
+
+	const [selected, setSelected] = useState<number[]>([])
+
 	const { width, height } = useMemo(() => {
 		return {
 			width: region.x2 - region.x1,
@@ -42,7 +51,7 @@ export function SplineEditor({ region, points, setPoints, axis }: Props) {
 	}, [region, width, height])
 
 	const setPoint = useCallback((j: number, point: Partial<Point>) => {
-		setPoints(points.map((p, i) => {
+		setPoints(points => points.map((p, i) => {
 			if (i !== j) return p
 			const minX = points[i-1]?.x ?? -Infinity
 			const maxX = points[i+1]?.x ?? Infinity
@@ -52,7 +61,7 @@ export function SplineEditor({ region, points, setPoints, axis }: Props) {
 				slope: point.slope ?? p.slope,
 			}
 		}))
-	}, [points])
+	}, [])
 
 	const segments = useMemo(() => {
 		return points.slice(1).map((next, i) => {
@@ -97,37 +106,67 @@ export function SplineEditor({ region, points, setPoints, axis }: Props) {
 
 	const mouseMove = useCallback((event: MouseEvent) => {
 		if (drag.current !== undefined && svg.current) {
-			const i = drag.current.index
-			const [x, y] = computePoint(event)
-			if (drag.current.type === 'point') {
-				setPoint(i, { x, y })
+			if (drag.current.type === 'selection') {
+				const [x, y] = computePoint(event)
+				const d = distance(X(x), Y(y), X(drag.current.x), Y(drag.current.y))
+				setBox(d === 0 ? undefined : { x1: drag.current.x, y1: drag.current.y, x2: x, y2: y })
 			} else {
-				const slope = (y - points[i].y) / (x - points[i].x)
-				setPoint(i, { slope })
+				setBox(undefined)
+				const i = drag.current.index
+				const [x, y] = computePoint(event)
+				if (drag.current.type === 'point') {
+					if (drag.current.rels) {
+						for (const [j, s] of drag.current.rels.entries()) {
+							if (i === j) continue
+							setPoint(j, { x: x + s.x, y: y + s.y })
+						}
+					}
+					setPoint(i, { x, y })
+				} else {
+					const slope = (y - points[i].y) / (x - points[i].x)
+					setPoint(i, { slope })
+				}
 			}
 		}
-	}, [points, computePoint, setPoint])
+	}, [points, selected, computePoint, setPoint, S, X, Y])
 
 	const mouseUp = useCallback(() => {
+		if (drag.current?.type === 'selection') {
+			if (box) {
+				setSelected(points.flatMap((p, i) => p.x >= Math.min(box.x1, box.x2) && p.x <= Math.max(box.x1, box.x2) && p.y >= Math.min(box.y1, box.y2) && p.y <= Math.max(box.y1, box.y2) ? [i] : []))
+			} else {
+				setSelected([])
+			}
+		}
 		drag.current = undefined
-	}, [])
+		setBox(undefined)
+	}, [points, box])
 
-	const startDrag = useCallback((type: Drag['type'], index: number) => (event: MouseEvent) => {
+	const startDrag = useCallback((type: 'point' | 'handle', i: number) => (event: MouseEvent) => {
 		if (event.button === 0) {
-			drag.current = { type, index }
+			if (selected.length > 0 && selected.includes(i)) {
+				const rels = new Map(selected.map(s => [s, {
+					x: points[s].x - points[i].x,
+					y: points[s].y - points[i].y,
+				}]))
+				drag.current = { type, index: i, rels }
+			} else {
+				drag.current = { type, index: i }
+			}
 			mouseMove(event)
 		} else if (event.button === 2) {
-			if (type === 'point' && points[index].slope === 0) {
+			if (type === 'point' && points[i].slope === 0) {
 				if (points.length > 2) {
 					const newPoints = points.slice()
-					newPoints.splice(index, 1)
+					newPoints.splice(i, 1)
 					setPoints(newPoints)
 				}
 			} else {
-				setPoint(index, { slope: 0 })
+				setPoint(i, { slope: 0 })
 			}
 		}
-	}, [points, mouseMove, setPoints, setPoint])
+		event.stopPropagation()
+	}, [points, selected, mouseMove, setPoints, setPoint])
 
 	const splitSegment = useCallback((event: MouseEvent) => {
 		if (svg.current) {
@@ -137,15 +176,22 @@ export function SplineEditor({ region, points, setPoints, axis }: Props) {
 			newPoints.splice(insert, 0, { x, y, slope: 0 })
 			setPoints(newPoints)
 		}
+		event.stopPropagation()
 	}, [points, computePoint, setPoints])
 
-	return <svg ref={svg} width='1' height='1' viewBox='0 0 1 1' onContextMenu={onContextMenu} onMouseMove={mouseMove} onMouseUp={mouseUp} class='w-full h-full aspect-square bg-zinc-700'>
+	const startSelection = useCallback((event: MouseEvent) => {
+		const [x, y] = computePoint(event)
+		drag.current = { type: 'selection', x, y }
+	}, [computePoint])
+
+	return <svg ref={svg} width='1' height='1' viewBox='0 0 1 1' onContextMenu={onContextMenu} onMouseDown={startSelection} onMouseMove={mouseMove} onMouseUp={mouseUp} class='w-full h-full aspect-square bg-zinc-700'>
 		{axis && <>
 			<line x1={X(region.x1)} y1={Y(0)} x2={X(region.x2)} y2={Y(0)} class='stroke-zinc-500' style={{ strokeWidth: `${S(0.5)}px` }} />
-			{Array(100).fill(0).map((_, i) =>	
-				<line x1={X(i/10-5)} y1={Y(0)} x2={X(i/10-5)} y2={Y(0)+S(i % 10 === 0 ? 3 : 1.5)} class='stroke-zinc-500' style={{ strokeWidth: `${S(0.5)}px` }} />,
+			{[...Array(100)].map((_, i) => [X(i/10 - 5), i]).filter(([x]) => x >= 0 && x <= 1).map(([x, i]) =>	
+				<line x1={x} y1={Y(0)} x2={x} y2={Y(0)+S(i % 10 === 0 ? 3 : 1.5)} class='stroke-zinc-500' style={{ strokeWidth: `${S(0.5)}px` }} />,
 			)}
 		</>}
+
 		{handles.map((s, i) => <>
 			<path d={`M ${X(s.x0)} ${Y(s.y0)} L ${X(s.x1)} ${Y(s.y1)}`} class='stroke-orange-200' style={{ strokeWidth: `${S(1)}px` }} onMouseDown={startDrag('handle', i)} />
 			<path d={`M ${X(s.x2)} ${Y(s.y2)} L ${X(s.x3)} ${Y(s.y3)}`} class='stroke-orange-200' style={{ strokeWidth: `${S(1)}px` }} onMouseDown={startDrag('handle', i + 1)} />
@@ -155,6 +201,8 @@ export function SplineEditor({ region, points, setPoints, axis }: Props) {
 			<circle cx={X(s.x1)} cy={Y(s.y1)} r={S(1.5)} class='fill-orange-200' onMouseDown={startDrag('handle', i)} />
 			<circle cx={X(s.x2)} cy={Y(s.y2)} r={S(1.5)} class='fill-orange-200' onMouseDown={startDrag('handle', i + 1)} />
 		</>)}
-		{points.map((p, i) => <circle cx={X(p.x)} cy={Y(p.y)} r={S(2)} class='fill-orange-300' onMouseDown={startDrag('point', i)} />)}
+		{points.map((p, i) => <circle cx={X(p.x)} cy={Y(p.y)} r={S(2)} class={selected.includes(i) ? 'fill-sky-400' : 'fill-orange-300'} onMouseDown={startDrag('point', i)} />)}
+
+		{box && <path d={`M ${X(box.x1)} ${Y(box.y1)} L ${X(box.x1)} ${Y(box.y2)} L ${X(box.x2)} ${Y(box.y2)} L ${X(box.x2)} ${Y(box.y1)} L ${X(box.x1)} ${Y(box.y1)}`} class='stroke-sky-600 fill-sky-900 opacity-30' fill='none' style={{ strokeWidth: `${S(1)}px` }} stroke-linejoin='round' stroke-linecap='round' />}
 	</svg>
 }
